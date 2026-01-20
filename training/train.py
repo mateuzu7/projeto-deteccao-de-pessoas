8ecdf672109d699d7041dc2a42fa9ac7b4c293f0
mateuzu7 <166437695+mateuzu7@users.noreply.github.com>
2026-01-20 13:52:45 -0300

    Add training script for Mask R-CNN model

---
+++ training/train.py
@@ -0,0 +1,21 @@
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = ("pessoas_train",)
+cfg.DATASETS.TEST = ("pessoas_valid",) # Validação durante o treino
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+
+cfg.SOLVER.IMS_PER_BATCH = 2
+cfg.SOLVER.BASE_LR = 0.00025
+cfg.SOLVER.MAX_ITER = 1000 # Quantidade boa para 94 fotos
+cfg.SOLVER.STEPS = []
+
+# CORREÇÃO: Definindo 2 classes (0: objects, 1: pessoas)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2
+
+cfg.OUTPUT_DIR = "/content/output"
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+
+trainer = DefaultTrainer(cfg)
+trainer.resume_or_load(resume=False)
+trainer.train()
